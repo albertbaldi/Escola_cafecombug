aa709fbb367b58055da6bfd11695620165c591a4
Albert (Windows) <albertbaldi@gmail.com>
2026-03-31 21:27:08 -0300

    Initial EF Core setup: context, configs, and repositories
    Added ApplicationDbContext with DbSets for all domain entities.
    Implemented entity configurations using Fluent API for Curso, Matricula, Nota, Turma, and Usuario.
    Stubbed repository classes for each entity, implementing interfaces but not yet providing method logic.
    Added Microsoft.EntityFrameworkCore package reference.
    This establishes the foundation for EF Core data access.

---
+++ Escola.Infra.Data/Repositories/MatriculaRepository.cs
@@ -0,0 +1,32 @@
+﻿using Escola.Domain.Entities;
+using Escola.Domain.Interfaces;
+
+namespace Escola.Infra.Data.Repositories;
+
+public class MatriculaRepository : IMatriculaRepository
+{
+    public Task<Matricula> AddAsync(Matricula matricula)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Matricula> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Matricula>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Matricula> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Matricula> UpdateAsync(Matricula matricula)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Escola.Infra.Data/Repositories/NotaRepository.cs
@@ -0,0 +1,32 @@
+﻿using Escola.Domain.Entities;
+using Escola.Domain.Interfaces;
+
+namespace Escola.Infra.Data.Repositories;
+
+public class NotaRepository : INotaRepository
+{
+    public Task<Nota> AddAsync(Nota nota)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Nota> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Nota>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Nota> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Nota> UpdateAsync(Nota nota)
+    {
+        throw new NotImplementedException();
+    }
+}
